96df7fc3edf8e6dfd597cbb173e8c8a297993159
subalterngames <subalterngames@gmail.com>
2021-04-02 16:13:36 -0400

    Version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from pathlib import Path
 setup(
     name="py_md_doc",
     packages={"py_md_doc"},
-    version="0.1.9",
+    version="0.1.10",
     license="MIT",
     description="Generate markdown documentation for your Python scripts."
                 "Like Sphinx, but simpler and directly compatible with GitHub.",
